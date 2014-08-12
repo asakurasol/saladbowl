95c5dadfe7547a49865ee6100b61bb924562856d
asakurasol <mamamia1734@gmail.com>
2014-08-11 19:14:30 -0700

    first commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,50 @@
+var Book = Backbone.Model.extend({
+	idAttribute: "_id"
+});
+
+var BookCollection = Backbone.Collection.extend({
+	model: Book,
+	url: "/books"
+});
+
+var BookView = Backbone.View.extend({
+	tagName: "li",
+	className: "book",
+	render: function(){
+		var template = $('#bookTemplate').html();
+		var compiled = Handlebars.compile(template);
+
+		var html = compiled(this.model.attributes);
+		this.$el.html(html);
+		return this;
+	}
+})
+
+var BookCollectionView = Backbone.View.extend({
+	initialize: function(){
+		this.listenTo(this.collection, "reset", this.render)
+	},
+	tagName : "ul",
+	className: "books",
+	render: function(){
+		console.log(this);
+		this.$el.html('');
+		this.collection.each(function(book){
+			var bookView = new BookView({ model: book})
+			this.$el.append(bookView.render().el);
+		}, this);
+		return this;
+	}
+})
+
+var AppRouter = Backbone.Router.extend({
+	routes: {
+		"": "index"
+	},
+	index: function(){
+		var collection = new BookCollection();
+		collection.fetch({reset: true})
+		var view = new BookCollectionView({collection:collection});
+		$(".app").html(view.render().el);
+	}
+})
